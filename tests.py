2371ee3879b8f86800ba2f380133594b9ec052e0
maxisoft <maxisoft.eu@gmail.com>
2016-02-09 17:02:31 +0100

    use better tests naming convention

--- tests.py
+++ tests.py
@@ -6,7 +6,7 @@ import unittest
 if __name__ == '__main__':
     path = os.path.join(os.path.dirname(__file__), "freenom_dns_updater")
     sys.path.insert(1, path)
-    test_files = glob.glob(os.path.join("freenom_dns_updater", 'test', '*test*.py'), recursive=True)
+    test_files = glob.glob(os.path.join("freenom_dns_updater", 'test', 'test_*.py'), recursive=True)
     module_strings = [test_file[0:len(test_file)-3].replace(os.sep, '.') for test_file in test_files]
     suites = [unittest.defaultTestLoader.loadTestsFromName(test_file) for test_file in module_strings]
     testSuite = unittest.TestSuite(suites)
