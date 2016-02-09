e0aef1e2fd3da0708291c13c5d6f6b4ae925c387
maxisoft <maxisoft.eu@gmail.com>
2016-02-09 18:16:10 +0100

    using another python multiversion support lib

--- freenom_dns_updater/test/test_freenom.py
+++ freenom_dns_updater/test/test_freenom.py
@@ -3,15 +3,14 @@ import unittest
 import requests
 from freenom_dns_updater import Freenom
 import os
-from past.builtins import basestring
+import six
 
 
 class FreenomTest(unittest.TestCase):
-
     def setUp(self):
         self.freenom = Freenom()
-        self.login = os.getenv("FREENOM_LOGIN", "default") # TODO
-        self.password = os.getenv("FREENOM_PASSWORD", "default") # TODO
+        self.login = os.getenv("FREENOM_LOGIN", "default")  # TODO
+        self.password = os.getenv("FREENOM_PASSWORD", "default")  # TODO
 
     def test_init(self):
         self.assertIsInstance(self.freenom.session, requests.Session)
@@ -24,7 +23,7 @@ class FreenomTest(unittest.TestCase):
 
     def test__get_login_token(self):
         result = self.freenom._get_login_token()
-        self.assertIsInstance(result, basestring)
+        self.assertIsInstance(result, six.string_types)
         self.assertTrue(result)
 
     def test_is_logged_in(self):
@@ -33,9 +32,10 @@ class FreenomTest(unittest.TestCase):
         self.assertTrue(self.freenom.is_logged_in())
 
     def test__get_login_token_no_token(self):
-        self.assertRaisesRegex(AssertionError,
-                               "there's no token",
-                               self.freenom._get_login_token, "http://httpbin.org/html")
+        six.assertRaisesRegex(self,
+                              AssertionError,
+                              "there's no token",
+                              self.freenom._get_login_token, "http://httpbin.org/html")
 
 
 if __name__ == '__main__':
