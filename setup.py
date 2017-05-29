1e771bb23f58ab19e7ab1200d73d199872ba4b3e
maxisoft <maxisoft.eu@gmail.com>
2017-05-29 12:22:15 +0200

    updated to 1.1.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1',
+    version='1.1.0',
 
     description="A tool to update freenom's dns records",
     long_description=long_description,
@@ -34,7 +34,7 @@ setup(
 
     # Author details
     author='maxisoft',
-    author_email='@maxisoft',
+    author_email='maxisoft@maxisoft.ga',
 
     # Choose your license
     license='MIT',
@@ -45,7 +45,7 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
